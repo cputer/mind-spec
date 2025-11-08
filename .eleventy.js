52b49a3319489ca6de1ed0ed6ebb54805afa9805
Nikolai Nedovodin <info@cputer.com>
2025-11-08 02:56:51 -0800

    ci(pages): Node 24 compatible Eleventy build to dist + GH Pages workflow

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,13 @@
 try{require("dotenv").config()}catch{}
-const MarkdownIt = require("markdown-it");
-
-module.exports = function(eleventyConfig) {
-  const md = new MarkdownIt({ html:true, linkify:true, typographer:true });
-  eleventyConfig.addPairedNunjucksShortcode("markdownRender", (content)=> md.render(content || ""));
-
+const MarkdownIt=require("markdown-it");
+module.exports=function(eleventyConfig){
+  const md=new MarkdownIt({html:true,linkify:true,typographer:true});
+  eleventyConfig.addPairedNunjucksShortcode("markdownRender",(c)=>md.render(c||""));
   return {
-    dir: { input: "src", includes: "_includes", data: "_data", output: "dist" },
-    templateFormats: ["njk","md","html"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-    dataTemplateEngine: "njk"
+    dir:{input:"src",includes:"_includes",data:"_data",output:"dist"},
+    templateFormats:["njk","md","html"],
+    htmlTemplateEngine:"njk",
+    markdownTemplateEngine:"njk",
+    dataTemplateEngine:"njk"
   };
 };
